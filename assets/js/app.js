edaf287a5eafd9952b3d0f64dd533e1ba0ba958f
Jayvant Pawar <pawarjayvant16@gmail.com>
2022-12-23 11:29:39 +0530

    Added languages

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,6 +14,7 @@ countries.forEach(function (data) {
             <h2 class="text-center text-warning text-uppercase">${data.name}</h2>
             <h3 class="text-secondary ml-3 col-md-12">Capital : ${data.capital}</h3>
             <h3 class="text-secondary ml-3 col-12">Population : ${data.population}</h3>
+            <h3 class="text-secondary ml-3 col-12">Languages : ${data.languages}</h3>
             </figcaption>
             </figure>
             </div>
